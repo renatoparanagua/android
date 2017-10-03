c28bd5f5a57266e2f9925a69576dfcb840b4ebe6
danilosetubal <danilosetubal@gmail.com>
2017-10-03 00:14:35 -0300

    Parte 1 rec ok

--- TimesRecAv2/app/src/main/java/com/projeto/danilosetubal/timesrecav2/MainActivity.java
+++ TimesRecAv2/app/src/main/java/com/projeto/danilosetubal/timesrecav2/MainActivity.java
@@ -1,7 +1,13 @@
 package com.projeto.danilosetubal.timesrecav2;
 
+import android.content.Intent;
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            String nome = extras.getString("nome");
+            Integer idade = extras.getInt("idade");
+            String time = extras.getString("Time");
+            if (nome != "") {
+            }
+        }
     }
-}
+
+    public void novo(View v) {
+        Intent intent = new Intent(this, CadastroTorcedorActivity.class);
+        startActivity(intent);
+    }
+
+    }
